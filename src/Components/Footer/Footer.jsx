8cdd3b9c931fc0395c819a8e8f3bbfa49b37c722
Abdul Hasib <abdulhasib2030@gmail.com>
2024-10-26 01:29:47 +0600

    footer section done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className='md:mt-0 mt-4'>
                 <img src={footerLogo} className='mx-auto' alt="" />
             </div>
-            <div className='md:flex justify-between md:container mt-9 mx-auto w-[90%]'>
+            <div className='md:flex justify-between md:container mt-9 mx-auto w-[90%] mb-6'>
                 <div className='space-y-3'> 
                     <h4 className='font-bold text-xl'>About Us</h4>
                     <p className='text-[#FFFFFF99] '>We are a passionate team <br /> dedicated to providing the best <br /> services to our customers.</p>
@@ -30,6 +30,10 @@ const Footer = () => {
                     <button className="font-bold bg-gradient-to-r from-pink-500 to-orange-500 py-3 px-6 rounded-r-xl">Subscribe</button>
                     </div>
                 </div>
+            </div>
+<hr style={{'border':'1px solid gray'}}/>
+            <div className='text-center mt-6 text-[#FFFFFF99]'>
+                @2024 BPL-DREAM30 All Rights Reserved.
             </div>
         </footer>
     );
